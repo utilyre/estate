ac5122684dc86934f79e8527bed7676c77d40208
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-05-01 00:14:14 +0430

    Move App to components directory

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './main.css'
-import App from './App'
+import { App } from './components'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
