ba51cf0197b29cc5861e3b14f455b506f60fa5ac
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-05-01 00:19:13 +0430

    Center App contents

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
+import styles from './App.module.css'
+
 const App = () => {
   return (
-    <main>
+    <main className={styles.container}>
       <h1>Commission</h1>
       <p>Hello world</p>
     </main>
