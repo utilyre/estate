e402bbb35d34e2973db8551398a311e6bb318669
Amirabbas Fazelinia <utilyre@proton.me>
2022-06-28 23:02:49 +0430

    Make sure to deploy the latest commit other than the current state
    Also wrapped the gh-pages work in parentheses so it'll cd out of the
    directory automatically

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,29 @@
 # Store the most recent commit hash for commit message
 commit_hash="$(git rev-parse "HEAD")"
 
+# Stash all modifications
+[ "$(git stash push --include-untracked)" = "No local changes to save" ] &&
+	stashed="false" ||
+	stashed="true"
+
 # Build the app
 npm run build
-cd "dist"
 
-# Initialize git repo
-git rev-parse --is-inside-work-tree > "/dev/null" && git init
-git branch --force --move "gh-pages"
+(
+	cd "dist"
 
-# Commit the distribution
-git add --all
-git commit --message="Deploy \`$commit_hash\`"
+	# Initialize git repo
+	git rev-parse --is-inside-work-tree > "/dev/null" && git init
+	git branch --force --move "gh-pages"
 
-# Push to https://utilyre.github.io/commission
-[ "$(git ls-remote --get-url "origin")" = "origin" ] && git remote add "origin" "git@github.com:utilyre/commission.git"
-git push --force "origin" "gh-pages"
+	# Commit the distribution
+	git add --all
+	git commit --message="Deploy \`$commit_hash\`"
+
+	# Push to https://utilyre.github.io/commission
+	[ "$(git ls-remote --get-url "origin")" = "origin" ] && git remote add "origin" "git@github.com:utilyre/commission.git"
+	git push --force "origin" "gh-pages"
+)
+
+# Bring back the modifications
+"$stashed" && git stash pop
