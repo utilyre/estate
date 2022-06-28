016475e00581db435fa73d862d699146a9533b8e
Amirabbas Fazelinia <utilyre@proton.me>
2022-06-28 22:07:19 +0430

    fix: Have the dist repo metadata more make sense
    - Changed branch name from `main` to `gh-pages`
    - Include current commit hash in the dist commit message

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env sh
 
+# Store the most recent commit hash for commit message
+commit_hash="$(git rev-parse "HEAD")"
+
 # Build the app
 npm run build
 cd "dist"
 
 # Initialize git repo
 git rev-parse --is-inside-work-tree > "/dev/null" && git init
-git branch --force --move "main"
+git branch --force --move "gh-pages"
 
 # Commit the distribution
 git add --all
-git commit --message="deploy"
+git commit --message="Deploy \`$commit_hash\`"
 
 # Push to https://utilyre.github.io/commission
-git push --force "git@github.com:utilyre/commission.git" "main:gh-pages"
+[ "$(git ls-remote --get-url "origin")" = "origin" ] && git remote add "origin" "git@github.com:utilyre/commission.git"
+git push --force "origin" "gh-pages"
