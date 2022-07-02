051982bef7824320342e400c09c821900af46133
Amirabbas Fazelinia <utilyre@proton.me>
2022-07-02 12:02:12 +0430

    Set base to `/commission/` in pwa config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 const pwaConfig = {
+  base: '/commission/',
   includeAssets: ['favicon.svg'],
   manifest: {
     name: 'Commission Calculator',
@@ -11,12 +12,12 @@ const pwaConfig = {
     theme_color: '#000000',
     icons: [
       {
-        src: '/commission-512x512.png',
+        src: '/commission/commission-512x512.png',
         sizes: '512x512',
         type: 'image/png',
       },
       {
-        src: '/commission-192x192.png',
+        src: '/commission/commission-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       },
