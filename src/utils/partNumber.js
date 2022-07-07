8c560cafac17e3e77d9de6709c5d1b3d263e07b2
Amirabbas Fazelinia <utilyre@proton.me>
2022-07-07 18:18:20 +0430

    Format using prettier

--- src/utils/partNumber.js
+++ src/utils/partNumber.js
@@ -1,6 +1,6 @@
 const partNumber = (number) => {
   const commaSeparatedNumber = number.toLocaleString('en-US')
-  const truncatedNumber = commaSeparatedNumber.split(".")[0]
+  const truncatedNumber = commaSeparatedNumber.split('.')[0]
 
   return truncatedNumber
 }
