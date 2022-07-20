f4fd278cd5af006caaa78eee2f698ce2216550de
Amirabbas Fazelinia <utilyre@proton.me>
2022-07-20 13:05:20 +0430

    Configure react-toastify

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 
 import './main.css'
 import { App } from './components'
@@ -7,5 +9,12 @@ import { App } from './components'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+
+    <ToastContainer
+      rtl
+      theme='dark'
+      position='top-left'
+      hideProgressBar
+    />
   </StrictMode>
 )
