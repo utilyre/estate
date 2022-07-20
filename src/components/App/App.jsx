2574088fb151decf15fa87ab3f657e07b6580618
Amirabbas Fazelinia <utilyre@proton.me>
2022-07-20 12:17:46 +0430

    Replace Selling and Renting with Calculator component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,52 @@
+import { useState } from 'react'
+
 import styles from './App.module.css'
-import { Selling, Renting } from '../../components'
+import { Calculator } from '../../components'
 
 const App = () => {
+  const [price, setPrice] = useState('')
+
+  const [mortgage, setMortgage] = useState('')
+  const [rent, setRent] = useState('')
+
   return (
     <main className={styles.container}>
-      <Selling />
-      <Renting />
+      <Calculator
+        title='خرید و فروش'
+        inputs={[
+          {
+            state: price,
+            setState: setPrice,
+            placeholder: 'قیمت (به تومان)',
+          },
+        ]}
+        calculateWage={(price) => {
+          const difference = price - 200_000_000
+          const overlap = (Math.abs(difference) + difference) / 2
+
+          return (price - overlap) * 0.005 + overlap * 0.0008
+        }}
+      />
+
+      <Calculator
+        title='رهن و اجاره'
+        inputs={[
+          {
+            state: mortgage,
+            setState: setMortgage,
+            placeholder: 'رهن (به تومان)',
+          },
+          {
+            state: rent,
+            setState: setRent,
+            placeholder: 'اجاره (به تومان)',
+          },
+        ]}
+        calculateWage={(mortgage, rent) => {
+          const half = (mortgage * 0.03 + rent) / 2
+          return half / 4 + half / 15
+        }}
+      />
     </main>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,7 @@
 import App from './App/App'
 import Calculator from './Calculator/Calculator'
-import Selling from './Selling/Selling'
-import Renting from './Renting/Renting'
 
 export {
   App,
   Calculator,
-  Selling,
-  Renting,
 }
