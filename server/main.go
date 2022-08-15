93b9c4499a3e25155e28945fb34769cc194d3d01
Amirabbas Fazelinia <utilyre@proton.me>
2022-08-15 21:49:36 +0430

    build(air): configure air

--- server/main.go
+++ server/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello world!")
+	})
+
+	app.Listen(":" + os.Getenv("SERVER_PORT"))
 }
